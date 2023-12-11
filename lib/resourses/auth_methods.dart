a7607a17d04aa28741dccb54a630357b73e98263
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2023-12-11 17:54:09 +0545

    done until file added to storage

--- lib/resourses/auth_methods.dart
+++ lib/resourses/auth_methods.dart
@@ -4,45 +4,46 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class AuthMethods{
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-   Future<String>signUpUser({
+  Future<String> signUpUser({
     required String username,
     required String password,
     required String email,
     required String bio,
-    // required Uint8List file,
-   })async{
-      String res = "Some Error Occoured";
+    required Uint8List file,
+  }) async {
+    String res = "Some Error Occoured";
 
-      try{
-// ||file!=null
-        if(username.isNotEmpty||password.isNotEmpty||email.isNotEmpty||bio.isNotEmpty ){
+    try {
+      if (username.isNotEmpty ||
+          password.isNotEmpty ||
+          email.isNotEmpty ||
+          bio.isNotEmpty ||
+          file != null) {
+        //Regestering The Users
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
 
-          //Regestering The Users
-           UserCredential cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-
-           print(cred.user!.uid);
+        print(cred.user!.uid);
         // Add users to databaseee
         _firestore.collection("users").doc(cred.user!.uid).set({
-        'username' : username,
-        'password': password,
-        'uid':cred.user!.uid,
-        'email':email,
-        'bio':bio,
-        'followers':[],
-        'following':[],
-         } );
+          'username': username,
+          'password': password,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+        });
 
-         res = "Success";
-        
-        }
-      }catch(err){
-        res = err.toString();
+        res = "Success";
       }
-      return res;
-   }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
 }
