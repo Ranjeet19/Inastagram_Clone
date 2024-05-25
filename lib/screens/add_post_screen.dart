01553315c0fec9371a36d035ee41ce20ed8f79a5
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2024-05-25 11:35:56 +0545

    Add post UI part done 3:9

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:instagram_app/utils/colors.dart';
 
 class AddPostScreen extends StatefulWidget {
@@ -23,10 +24,10 @@ class _AddPostScreenState extends State<AddPostScreen> {
       appBar: AppBar(
         backgroundColor: mobileBackgroundColor,
         leading: IconButton(
-          icon: Icon(CupertinoIcons.back),
+          icon:const Icon(CupertinoIcons.back),
           onPressed: () {},
         ),
-        title: Text("Post to"),
+        title: const Text("Post to"),
         centerTitle: false,
         actions: [
           TextButton(
@@ -43,8 +44,46 @@ class _AddPostScreenState extends State<AddPostScreen> {
       ),
       body: Column(
         children: [
+          const SizedBox(
+            height: 10,
+          ),
           Row(
-            children: [],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(width: 5,),
+              const CircleAvatar(
+                backgroundImage: NetworkImage("url"),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.65,
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Write a Caption...",
+                    border: InputBorder.none,
+                  ),
+                  maxLines: 8,
+                ),
+              ),
+              SizedBox(
+                height: 45,
+                width: 45,
+                child: AspectRatio(
+                  aspectRatio: 487 / 451,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                      image: NetworkImage(
+                        "https://images.unsplash.com/photo-1636564499112-6113e73c504a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                      ),
+                      fit: BoxFit.fill,
+                      alignment: FractionalOffset.topCenter,
+                    )),
+                  ),
+                ),
+              ),
+              const Divider(),
+            ],
           )
         ],
       ),
