2b3431eac0033bc75152d018ccc5411ab57f8fb1
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2024-05-31 23:21:18 +0545

    completed till PostCard UI 4:10

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:instagram_app/utils/colors.dart';
 
 class PostCard extends StatelessWidget {
@@ -40,14 +42,17 @@ class PostCard extends StatelessWidget {
                           context: context,
                           builder: (context) => Dialog(
                                 child: ListView(
-                                  padding: const EdgeInsets.symmetric(vertical: 16),
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 16),
                                   shrinkWrap: true,
                                   children: ['Delete']
                                       .map((e) => InkWell(
                                             onTap: () {},
                                             child: Container(
-                                              padding: const EdgeInsets.symmetric(
-                                                  vertical: 12, horizontal: 16),
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 12,
+                                                      horizontal: 16),
                                               child: Text(e),
                                             ),
                                           ))
@@ -58,7 +63,89 @@ class PostCard extends StatelessWidget {
                     icon: const Icon(Icons.more_vert)),
               ],
             ),
-          )
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network("https://images.unsplash.com/photo-1585020430145-2a6b034f7729?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", fit: BoxFit.cover),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_outline),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.mode_comment_outlined),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.send),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.bookmark_outline),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Descriptions and number of comments,
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DefaultTextStyle(
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium!
+                        .copyWith(fontWeight: FontWeight.w800),
+                    child: Text(
+                      '123 likes',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    )),
+
+                    Container(
+                      width: double.infinity,
+                      padding: const EdgeInsets.only(top: 8),
+                      child:  RichText(text: const TextSpan(
+                        style: TextStyle(color: primaryColor),
+                        children: [
+                          TextSpan(
+                            text: "Username ",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: "Hey this is the description which is supposed to be replaced verry soon"
+                          )
+                      ]),),
+
+
+                    ),
+                    InkWell(
+                      onTap: (){},
+                      child: Container(
+                        padding:  const EdgeInsets.only(top: 5),
+                        child: Text("View all 200 Comments",
+                        style: TextStyle(color: secondaryColor),),
+                      ),
+                    ),
+                    Container(
+                        padding:  const EdgeInsets.symmetric(vertical: 1),
+                        child: Text("22/09/2024",
+                        style: TextStyle(color: secondaryColor, fontSize: 14),),
+                      ),
+
+
+              ],
+            ),
+          ),
         ],
       ),
     );
