7bdf001a47b17778868f9d6ef02ef74a433d3c76
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2024-06-04 20:44:22 +0545

    Searching Users part is completed 5:39

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -3,12 +3,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_app/screens/add_post_screen.dart';
 import 'package:instagram_app/screens/feed_screen.dart';
+import 'package:instagram_app/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
   FeedScreen(),
-  Text("Search"),
+  SearchScreen(),
   AddPostScreen(),
   Text("Faviroute"),
   Text("Profile"),
