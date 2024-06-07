3827d875785fa7d9148905c7e8680376203832f2
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2024-06-07 23:49:15 +0545

    Profile UI part done 6:5

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,118 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:instagram_app/utils/colors.dart';
+import 'package:instagram_app/widgets/follow_button.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: Text('username'),
+        centerTitle: false,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage("urls"),
+                      radius: 40,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              buildStateColumn(20, "Posts"),
+                              buildStateColumn(2032, "followers"),
+                              buildStateColumn(1500, "following"),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              FollowButton(
+                                backgroundColor: mobileBackgroundColor,
+                                borderColor: primaryColor,
+                                text: "Edit Profile",
+                                textColor: Colors.grey,
+                                function: () {},
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(top: 15),
+                  child: Text(
+                    "username",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: EdgeInsets.only(top: 1),
+                  child: Text(
+                    "Some Useful Descriptions",
+                  ),
+                ),
+                const Divider(),
+              ],
+            ),
+          ),
+          // const Divider(),
+        ],
+      ),
+    );
+  }
+
+  Column buildStateColumn(int num, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          num.toString(),
+          style: const TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 4),
+          child: Text(
+            label.toString(),
+            style: const TextStyle(
+                fontSize: 15, fontWeight: FontWeight.w400, color: Colors.grey),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_app/screens/add_post_screen.dart';
 import 'package:instagram_app/screens/feed_screen.dart';
+import 'package:instagram_app/screens/profile_screen.dart';
 import 'package:instagram_app/screens/search_screen.dart';
 
 const webScreenSize = 600;
@@ -12,5 +13,4 @@ const homeScreenItems = [
   SearchScreen(),
   AddPostScreen(),
   Text("Faviroute"),
-  Text("Profile"),
-];
+ProfileScreen()];
