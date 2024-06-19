3fd0ea9cc8b3c0e30418221a2384745674209f86
Ranjeet Shrestha <shrestharanjit2014@gmail.com>
2024-06-19 22:56:48 +0545

    Setup done for Android mob working fine

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'firebase_options.dart';
 // ...
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
